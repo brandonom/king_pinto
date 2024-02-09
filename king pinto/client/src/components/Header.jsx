c08ba18313d5402c12651222dd08b5290c9e4af7
alex pohlman <apohlman53@gmail.com>
2024-02-09 15:06:12 -0500

    createde sign in/register

--- king pinto/client/src/components/Header.jsx
+++ king pinto/client/src/components/Header.jsx
@@ -5,12 +5,8 @@ import { Disclosure } from "@headlessui/react";
 import { useMutation, useQuery } from "@apollo/client";
 import { NavLink, useNavigate, Link } from "react-router-dom";
 import { useStoreContext } from "../utils/store";
-import {
-  UPDATE_USER,
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-} from "../utils/actions";
-import { QUERY_AUTHENTICATE, QUERY_CATEGORIES } from "../utils/queries";
+import { UPDATE_USER } from "../utils/actions";
+import { QUERY_AUTHENTICATE } from "../utils/queries";
 import { LOGOUT } from "../utils/mutations";
 
 function classNames(...classes) {
@@ -19,7 +15,6 @@ function classNames(...classes) {
 
 export default function Header() {
   const [state, dispatch] = useStoreContext();
-  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
   const navigate = useNavigate();
   const [logoutUser] = useMutation(LOGOUT, {
     refetchQueries: [QUERY_AUTHENTICATE],
@@ -39,143 +34,109 @@ export default function Header() {
     }
   };
 
-  useEffect(() => {
-    if (categoryData) {
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories,
-      });
-    } else if (!loading) {
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categories,
-      });
-    }
-  }, [categoryData, loading, dispatch]);
-
-  const handleClick = (id) => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id,
-    });
-  };
-
-  const navigation = categoryData?.categories || [];
-
   return (
-    <header
-      style={{ backgroundColor: "bg-gray-600", padding: "10px" }}
-      className="flex items-center justify-between p-6 bg-white dark:bg-gray-800"
-    >
-      <h1 className="text-2xl font-bold">King Pinto</h1>
-      <nav className="flex gap-4">
-        <ul className="flex list-none">
-          <li
-            style={{ padding: "10px" }}
-            className="text-gray-600 hover:underline dark:text-gray-300"
+    <Disclosure as="nav" className="bg-black">
+      {({ open }) => (
+        <>
+          <header
+            style={{ backgroundColor: "bg-gray-600", padding: "10px" }}
+            className="flex items-center justify-between p-6 bg-white dark:bg-gray-800"
           >
-            <a href="/">Home</a>
-          </li>
-          <li
-            style={{ padding: "10px" }}
-            className="text-gray-600 hover:underline dark:text-gray-300"
-          >
-            <a href="#mural">Mural</a>
-          </li>
-          <li>
-            <a href="#gallery">Gallery</a>
-          </li>
-        </ul>
-        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          {state.user ? (
-            <>
-              <p className="welcome mr-3">Welcome, {state.user.firstName}</p>
-
-              {state.user.orders.length > 0 ? (
-                <NavLink
-                  to="/order-history"
-                  className="hidden md:block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+            <h1 className="text-2xl font-bold">King Pinto</h1>
+            <nav className="flex gap-4">
+              <ul className="flex list-none">
+                <li
+                  style={{ padding: "10px" }}
+                  className="text-gray-600 hover:underline dark:text-gray-300"
                 >
-                  Order History
-                </NavLink>
-              ) : (
-                ""
-              )}
-              <NavLink
-                to="/logout"
-                onClick={logout}
-                className="hidden md:block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-              >
-                Log Out
-              </NavLink>
-            </>
-          ) : (
-            <>
-              <NavLink
-                to="/login"
-                className="hidden md:block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-              >
-                Sign In
-              </NavLink>
-              <NavLink
-                to="/register"
-                className="hidden md:block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-              >
-                Register
-              </NavLink>
-            </>
-          )}
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Link
-                  key={item.name}
-                  to="/products"
-                  onClick={() => {
-                    handleClick(item._id);
-                  }}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
+                  <a href="/">Home</a>
+                </li>
+                <li
+                  style={{ padding: "10px" }}
+                  className="text-gray-600 hover:underline dark:text-gray-300"
                 >
-                  {item.name}
-                </Link>
-              ))}
+                  <a href="#mural">Mural</a>
+                </li>
+                <li>
+                  <a href="#gallery">Gallery</a>
+                </li>
+              </ul>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {state.user ? (
+                  <>
+                    <p className="welcome mr-3">
+                      Welcome, {state.user.firstName}
+                    </p>
 
-              {state.user ? (
-                <>
-                  <NavLink
-                    to="/logout"
-                    onClick={logout}
-                    className="mt-10 text-gray-400 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-small"
-                  >
-                    Log Out
-                  </NavLink>
-                </>
-              ) : (
-                <>
-                  <NavLink
-                    to="/login"
-                    className="mt-10 text-gray-400 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-small"
-                  >
-                    Sign In
-                  </NavLink>
-                  <NavLink
-                    to="/register"
-                    className="mt-10 text-gray-400 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-small"
-                  >
-                    Register
-                  </NavLink>
-                </>
-              )}
-            </div>
-          </Disclosure.Panel>
-        </div>
-      </nav>
-    </header>
+                    {state.user.orders.length > 0 ? (
+                      <NavLink
+                        to="/order-history"
+                        className="hidden md:block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                      >
+                        Order History
+                      </NavLink>
+                    ) : (
+                      ""
+                    )}
+                    <NavLink
+                      to="/logout"
+                      onClick={logout}
+                      className="hidden md:block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    >
+                      Log Out
+                    </NavLink>
+                  </>
+                ) : (
+                  <>
+                    <NavLink
+                      to="/login"
+                      className="hidden md:block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    >
+                      Sign In
+                    </NavLink>
+                    <NavLink
+                      to="/register"
+                      className="hidden md:block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    >
+                      Register
+                    </NavLink>
+                  </>
+                )}
+                <Disclosure.Panel className="sm:hidden">
+                  <div className="space-y-1 px-2 pb-3 pt-2">
+                    {state.user ? (
+                      <>
+                        <NavLink
+                          to="/logout"
+                          onClick={logout}
+                          className="mt-10 text-gray-400 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-small"
+                        >
+                          Log Out
+                        </NavLink>
+                      </>
+                    ) : (
+                      <>
+                        <NavLink
+                          to="/login"
+                          className="mt-10 text-gray-400 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-small"
+                        >
+                          Sign In
+                        </NavLink>
+                        <NavLink
+                          to="/register"
+                          className="mt-10 text-gray-400 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-small"
+                        >
+                          Register
+                        </NavLink>
+                      </>
+                    )}
+                  </div>
+                </Disclosure.Panel>
+              </div>
+            </nav>
+          </header>
+        </>
+      )}
+    </Disclosure>
   );
 }
