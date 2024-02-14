12dcb7c78fab0902e869b40507eb6f25caa30e30
alex pohlman <apohlman53@gmail.com>
2024-02-14 17:40:17 -0500

    updated vite server

--- king pinto/vite.config.js
+++ king pinto/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/graphql": {
+        target: "http://localhost:3333",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    // Explicitly set the entry point
+    rollupOptions: {
+      input: "/src/main.jsx", // Adjust the path as needed
+    },
+  },
+});
